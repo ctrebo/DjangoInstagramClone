14d6b7e2e5af43e66d86b41e0e3c4e0ad3b1aa5d
Trebo Christoffer <christoffer.trebo@gmail.com>
2021-03-17 19:19:18 +0100

    add function that shows when post was posted

--- insta/models.py
+++ insta/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 from django.conf import settings
-
+import datetime
+from datetime import timezone, timedelta
 
 class CustomUser(AbstractUser):
     date_joined = models.DateTimeField(auto_now_add=True)
@@ -21,7 +22,7 @@ class Post(models.Model):
     caption = models.TextField(max_length=1500, blank=True, null=True)
 
     class Meta:
-        ordering = ["post_date"]
+        ordering = ["-post_date"]
 
     def __str__(self):
         return self.author.username
@@ -37,6 +38,50 @@ class Post(models.Model):
         return number of likes on post
         """
         return self.likes.count()
+    
+    @property
+    def time_posted_ago(self):
+        """
+        Function that return how long ago post was created
+        """
+
+        difference = datetime.datetime.now(timezone.utc) - self.post_date 
+        if difference.total_seconds() < 60 and difference.total_seconds() > 1:
+            result= str(difference.total_seconds())[:-7]
+            if result == "1":
+                return result + " second ago"
+            else:
+                return result + " second ago"                
+        elif difference.total_seconds()/60 < 60 and difference.total_seconds()/60 > 1:
+            result = str(divmod(difference.total_seconds(), 60)[0])[:-2]
+            if result == "1":
+                return result + " minute ago"
+            else:
+                return result + " minutes ago"   
+        elif difference.total_seconds()/(60*60) < 24 and difference.total_seconds()/(60*60) >= 1:
+            result = str(divmod(difference.total_seconds(), (60*60))[0])[:-2]
+            if result == "1":
+                return result + " hour ago"
+            else:
+                return result + " hours ago"  
+        elif difference.total_seconds()/(60*60*24) <= 30 and difference.total_seconds()/(60*24*60) >= 1:
+            result= str(divmod(difference.total_seconds(), (60*60*24))[0])[:-2]
+            if result == "1":
+                return result + " day ago"
+            else:
+                return result + " days ago"  
+        elif difference.total_seconds()/(2628002.88) < 12 and difference.total_seconds()/(2628002.88) >= 1:
+            result = str(divmod(difference.total_seconds(), 2628002.88)[0])[:-2]
+            if result == "1":
+                return result + " month ago"
+            else:
+                return result + " months ago"  
+        elif difference.total_seconds()/(31556952) >= 1:
+            result = str(divmod(difference.total_seconds(), 31556952)[0])[:-2]
+            if result == "1":
+                return result + " year ago"
+            else:
+                return result + " years ago"             
 
 class PostComment(models.Model):
     """
@@ -49,7 +94,7 @@ class PostComment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     
     class Meta:
-        ordering = ["post_date"]
+        ordering = ["-post_date"]
 
     def __str__(self):
         """
