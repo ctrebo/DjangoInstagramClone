f77b1c47df2630db152a96b5caec1045407cdc17
Trebo Christoffer <christoffer.trebo@gmail.com>
2021-03-20 19:46:28 +0100

    add function that returns when  postcomment was posted

--- insta/models.py
+++ insta/models.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 from django.conf import settings
 import datetime
 from datetime import timezone, timedelta
+from django.urls import reverse #Used to generate URLs by reversing the URL patterns
+
 
 class CustomUser(AbstractUser):
     date_joined = models.DateTimeField(auto_now_add=True)
@@ -17,7 +19,7 @@ class Post(models.Model):
 
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     post_date = models.DateTimeField(auto_now_add=True)
-    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='blogpost_like', blank=True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likes', blank=True)
     picture = models.ImageField(upload_to="images_post", height_field=None, width_field=None)
     caption = models.TextField(max_length=1500, blank=True, null=True)
 
@@ -106,3 +108,47 @@ class PostComment(models.Model):
         else:
             titlestring=self.description
         return titlestring
+
+    @property
+    def time_posted_ago(self):
+        """
+        Function that return how long ago post was created
+        """
+
+        difference = datetime.datetime.now(timezone.utc) - self.post_date 
+        if difference.total_seconds() < 60 and difference.total_seconds() > 1:
+            result= str(difference.total_seconds())[:-7]
+            if result == "1":
+                return result + " second ago"
+            else:
+                return result + " second ago"                
+        elif difference.total_seconds()/60 < 60 and difference.total_seconds()/60 > 1:
+            result = str(divmod(difference.total_seconds(), 60)[0])[:-2]
+            if result == "1":
+                return result + " minute ago"
+            else:
+                return result + " minutes ago"   
+        elif difference.total_seconds()/(60*60) < 24 and difference.total_seconds()/(60*60) >= 1:
+            result = str(divmod(difference.total_seconds(), (60*60))[0])[:-2]
+            if result == "1":
+                return result + " hour ago"
+            else:
+                return result + " hours ago"  
+        elif difference.total_seconds()/(60*60*24) <= 30 and difference.total_seconds()/(60*24*60) >= 1:
+            result= str(divmod(difference.total_seconds(), (60*60*24))[0])[:-2]
+            if result == "1":
+                return result + " day ago"
+            else:
+                return result + " days ago"  
+        elif difference.total_seconds()/(2628002.88) < 12 and difference.total_seconds()/(2628002.88) >= 1:
+            result = str(divmod(difference.total_seconds(), 2628002.88)[0])[:-2]
+            if result == "1":
+                return result + " month ago"
+            else:
+                return result + " months ago"  
+        elif difference.total_seconds()/(31556952) >= 1:
+            result = str(divmod(difference.total_seconds(), 31556952)[0])[:-2]
+            if result == "1":
+                return result + " year ago"
+            else:
+                return result + " years ago"   
