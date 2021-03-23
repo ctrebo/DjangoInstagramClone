c61d93422bd067dc859684ca4ecc2e44683c9212
Trebo Christoffer <christoffer.trebo@gmail.com>
2021-03-23 12:36:51 +0100

    fix wrong name bug

--- insta/views.py
+++ insta/views.py
@@ -1,15 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Post, PostComment
 
+from insta.forms import PostCommentCreateForm
+from django.contrib.auth import get_user_model
+
+
+
 User = settings.AUTH_USER_MODEL
 # Create your views here.
+
+# def BlogPostLike(request, pk):
+#     post = get_object_or_404(BlogPost, id=request.POST.get('blogpost_id'))
+#     if post.likes.filter(id=request.user.id).exists():
+#         post.likes.remove(request.user)
+#     else:
+#         post.likes.add(request.user)
+
+#     return HttpResponseRedirect(reverse('blogpost-detail', args=[str(pk)]))
 
 class PostListView(LoginRequiredMixin, generic.ListView):
     model = Post
@@ -17,16 +32,48 @@ class PostListView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         return Post.objects.exclude(author=self.request.user).order_by("-post_date")
     
+    def get_context_data(self, **kwargs):
+        user_model = get_user_model()
+        users = user_model.objects.all()[:10]
 
-    # def get_context_data(self, **kwargs):
-    #     """
-    #     Add BlogAuthor to context so they can be displayed in the template
-    #     """
-    #     # Call the base implementation first to get a context
-    #     # context = super(PostListView, self).get_context_data(**kwargs)
-    #     context['num_comments'] = PostComment.objects.filter(id=) 
-    #     return context
+        context = super(PostListView, self).get_context_data(**kwargs)
+        context["recommandation_list"] = users
+        return context
 
 
 
+@login_required
+def postCommentCreate(request, pk):
+    """View function for renewing a specific BookInstance by librarian."""
+    post_for_comment = get_object_or_404(Post, pk=pk)
+    comment_list = PostComment.objects.filter(post=post_for_comment).order_by("post_date")
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = PostCommentCreateForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            #create new object with data from form
+            comment=PostComment.objects.create(description=form.cleaned_data["description"], author=request.user, post=post_for_comment)
+
+            comment.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('index') )
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        comment = ""
+        form = PostCommentCreateForm(initial={"description":comment})
+
+    context = {
+        'form': form,
+        'post': post_for_comment,
+        'comment_list': comment_list, 
+    }
+
+    return render(request, 'insta/postcomment_form.html', context)
 
