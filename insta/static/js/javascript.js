86c3f417acad23cfd8518296587a7443eca17793
Trebo Christoffer <christoffer.trebo@gmail.com>
2021-04-02 11:06:09 +0200

    add copy to clipboard to homepage

--- insta/static/js/javascript.js
+++ insta/static/js/javascript.js
@@ -7,3 +7,9 @@ $("#comment-section").css("height", variable_height.toString() + "px");
 $( ".fontawesome-border" ).click(function() {
     $(this).css("text-shadow", "-1px 0 rgb(136, 128, 128), 0 1px rgb(136, 128, 128), 1px 0 rgb(136, 128, 128), 0 -1px rgb(136, 128, 128)")
   });
+
+function copyToClipboard(mydata) {
+    // var mydata = JSON.parse(document.getElementById('mydata').textContent);
+    navigator.clipboard.writeText(mydata);
+
+}
